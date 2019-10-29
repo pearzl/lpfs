b7414826c8de2eae6ecad0bed7bb24fc5c065942
pearzl <302669696@qq.com>
2019-10-29 23:06:33 +0800

    driver rtc

--- src/lib.rs
+++ src/lib.rs
@@ -282,6 +282,25 @@ pub fn diskstats() -> Result<Vec<Disk>> {
     Ok(ret)
 }
 
+pub mod driver {
+    use super::{Result, Error};
+    use std::collections::HashMap;
+
+    pub fn rtc() -> Result<HashMap<String, String>> {
+        let content = std::fs::read_to_string("/proc/driver/rtc")?;
+        let mut ret = HashMap::new();
+
+        for line in content.lines() {
+            let mut kv = line.split(':');
+            let key = kv.next().ok_or(Error::BadFormat)?;
+            let value = kv.next().ok_or(Error::BadFormat)?;
+            
+            ret.insert(key.trim().to_string(), value.trim().to_string());
+        }
+
+        Ok(ret)
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -292,6 +311,7 @@ mod test {
 //        println!("{:#?}", super::cpuinfo());        
 //        println!("{:#?}", super::consoles());        
 //        println!("{:#?}", super::cmdline());        
-        println!("{:#?}", super::diskstats());
+//        println!("{:#?}", super::diskstats());
+        println!("{:#?}", super::driver::rtc());
     }
 }
