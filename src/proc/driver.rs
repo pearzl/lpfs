13fb933ac47e3b122b5b80b87c2488fe3810500b
pearzl <302669696@qq.com>
2019-10-29 23:59:46 +0800

    refactor dirctory layout

---
+++ src/proc/driver.rs
@@ -0,0 +1,17 @@
+use super::{Error, Result};
+use std::collections::HashMap;
+
+pub fn rtc() -> Result<HashMap<String, String>> {
+    let content = std::fs::read_to_string("/proc/driver/rtc")?;
+    let mut ret = HashMap::new();
+
+    for line in content.lines() {
+        let mut kv = line.split(':');
+        let key = kv.next().ok_or(Error::BadFormat)?;
+        let value = kv.next().ok_or(Error::BadFormat)?;
+
+        ret.insert(key.trim().to_string(), value.trim().to_string());
+    }
+
+    Ok(ret)
+}
